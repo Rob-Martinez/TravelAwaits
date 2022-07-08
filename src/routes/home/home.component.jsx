5df2ba921f2b1e40fa658767a3cbb9d506f884e7
Rob-Martinez <86384534+Rob-Martinez@users.noreply.github.com>
2022-07-08 18:19:27 -0500

    added css to video background, added text overlay, added responsive text sizing. Currently everything looks good!

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,14 +1,23 @@
 import Navbar from "../../components/navbar/navbar.component";
-import Herovid from '../../assets/videos/video.mp4'
+import Herovid from '../../assets/videos/vid.mp4'
+
+import './home.styles.scss'
 
 
 const Home = () => {
     return(
         
         <div className="home-container">
-           <Navbar />
-           <div className="home-hero-container">
-            <img src={Herovid} alt="Beach shore from the air" />
+            <Navbar />
+            <div className="home-hero-container">
+                <video src={Herovid} alt="Beach shore from the air" autoPlay loop muted/>
+                <div className="hero-content">
+                    <h2>Adventure Awaits</h2>
+                    <p>What are you waiting for?</p>
+                    <button>Get Started</button>
+                </div>
+                
+            
            </div>
         </div>
     );
