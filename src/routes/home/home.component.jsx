07caa673498f51207f8172e8e0bc0cb073ccbedf
Rob-Martinez <86384534+Rob-Martinez@users.noreply.github.com>
2022-07-12 21:51:43 -0500

    added container component, container styles, modified the home component to have a section on it for destinations on it. Will be moved out to its component afterwards just needed to make sure it was working correctly now before moving.

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 
 import './home.styles.scss'
+import '../../components/container/container.styles.scss' // This will need to be moved to container component after testing.
 
 
 const Home = () => {
@@ -20,18 +21,27 @@ const Home = () => {
                     <Link to='#'><Button buttonType='primary' type='button'>Get Started</Button></Link>
                 </div>
            </div>
-           <div className="container">
-                <img src="" alt="" />
-                <h3>Title</h3>
-                <div>
-                    Lorem ipsum dolor, sit amet consectetur 
-                    adipisicing elit. Voluptatibus dolore consequuntur a
-                    rchitecto error incidunt autem dicta possimus sapiente l
-                    aborum eius, impedit, fugiat, excepturi cum molestias se
-                    qui obcaecati? Praesentium, aperiam minus.
+
+           <section className="section">
+                <div className="container">
+                    <div className="info column">
+                        <h2 className="title ">Balloon Festival, India</h2>
+                        <div className="description">
+                            <p>
+                                Lorem ipsum dolor, sit amet consectetur 
+                                adipisicing elit. Voluptatibus dolore consequuntur a
+                                rchitecto error incidunt autem dicta possimus sapiente l
+                                aborum eius, impedit, fugiat, excepturi cum molestias se
+                                qui obcaecati? Praesentium, aperiam minus.
+                            </p>
+                        </div>
+                        <div className="button-div">
+                            <Button buttonType='primary' type='button'>Test Button</Button>
+                        </div>
+                    </div>
+                        <img className='column' src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 </div>
-                <Button buttonType='primary' type='button'>Test Button</Button>
-           </div>
+           </section>
         </div>
     );
 }
