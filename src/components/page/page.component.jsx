2475974cb92a76790fc3e5e54ab40c3ae61d5ec7
Rob-Martinez <86384534+Rob-Martinez@users.noreply.github.com>
2022-07-21 20:36:58 -0500

    used link to pass data to the page component, added image,credit,creditLink to Place Object. Everything seems to be working as intended.

--- src/components/page/page.component.jsx
+++ src/components/page/page.component.jsx
@@ -2,75 +2,43 @@ import { Link } from "react-router-dom";
 import Button from "../button/button.components";
 import Footer from "../footer/footer.component";
 import Navbar from "../navbar/navbar.component";
+import { useLocation } from "react-router-dom";
 
 import './page.styles.scss';
 
 
-const PageContent = [
-    {
-        Centralia: {
-            title: "Centralia",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur molestias iusto",
-        },
 
-        FortBenning: {
-            title: "Fort Benning, GA",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur molestias iusto sdfs",
-        },
+    const Page = () => {
 
-        JacobsHole: {
-            title: "Jacobs Hole, TX",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur molestias ",
-        },
+    const location = useLocation();
+    const {place} = location.state;
+    console.log(place);
 
-        MountRainer: {
-            title: "Mount Rainer, WA",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur molestias iusto sdfssdf sd",
-        }
-    }
-]
+    const { title, description, image, credit, creditLink } = place;
+    console.log({title, description});
 
 
+    
 
-const Page = () => {
-    return(
+    return (
         <div className="page-container">
             <Navbar />
             <div>
                 <div className="image-container">
         
-                    <img src="https://cloudfront-us-east-1.images.arcpublishing.com/advancelocal/OHKE7SRWTFEODETIVYH5KTZ5CA.jpg"
+                    <img src={image}
                          alt="" 
                     />
                 
                 </div>
                 
                 <div className="page-content">
-                    <h2>Centralia, PA</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur 
-                        adipisicing elit. Tenetur molestias iusto 
-                        laborum totam sit et deleniti explicabo ipsa 
-                        harum itaque perspiciatis ipsum volupta
-                        adipisci, dignissimos tempore impedit iste
-                        eaque? Officia laudantium soluta minus ad
-                        ea placeat cumque ratione culpa inventore
-                        numquam mollitia nam, obcaecati saepe? A
-                        met aliquam, aspernatur necessitatibus nu
-                        mquam quaerat reprehenderit minima, dicta 
-                        corrupti expedita ut eligendi. Delectus te
-                        mpora veritatis ab, nesciunt quod culpa no
-                        n obcaecati enim neque earum error asperi
-                        res nihil, velit hic reiciendis reprehend
-                        erit est vel sapiente porro quae illo mag
-                        nam natus. Cumque commodi veritatis accusamus 
-                        quia doloribus labore iusto, doloremque ex inc
-                        idunt officia at id praesentium aspernatur imp
-                        edit facilis, ea consequuntur, assumenda voluptas
-                        vitae! Deleniti praesentium ipsam et rerum magnam
-                        illo, temporibus beatae, eveniet molestias debitis
-                        ipsum officia eos veniam! Voluptates adipisci eum qu
-                        i nesciunt expedita.
+                    <h2>{title}</h2>
+                    <p>{description}
+                    <br />
+                    Information provided by <a href={creditLink} target='blank'>{credit}</a>
                     </p>
+
                 </div>
                 
                 <Link to='/'><Button buttonType='primary' type='button'>Back</Button></Link>
--- src/components/section/section.component.jsx
+++ src/components/section/section.component.jsx
@@ -1,4 +1,5 @@
 import Button from '../button/button.components';
+import { Link } from 'react-router-dom';
 
 import './section.styles.scss';
 
@@ -6,37 +7,48 @@ import './section.styles.scss';
 const places = [
     {
         id: 1,
-        title: "Centralia, Pennsylvania",
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, cupiditate.'
+        title: "Graffiti Highway, Pennsylvania",
+        shortdesc: "After more than 60 years the coal fires under the small town of Centralia are still burning with no end in site...",
+        description: "In 1962, a few residents of Centralia, Pennsylvania harvested a fine crop of tomatoes. In December, the snow that winter all melted on the sidewalks. Why? Because a gigantic underground fire that is still burning 53 years later.First, some geology. Central Pennsylvania sits atop some of the largest coal deposits in the world, formed by millions of years of geological pressure. In the 1800s, miners begin tunneling beneath the ground there to harvest it. By the mid-1900s, demand for coal dropped and many of the Pennsylvania mines were abandoned.Back to 1962. No one quite knows how the Centralia fire started. The leading theory today is that burning trash near an old mine entrance accidentally ignited the coal beneath. Once it ignited, the fire began to spread.Coal burns when carbon inside it combines with oxygen. The tunnels provided oxygen from the surface. As more and more coal burned, the flames ate deeper and deeper into the surrounding terrain. And unlike wood in a forest fire, coal burns slowly and steadily so the fires didn't burn themselves out quickly.So what exactly is fire? Well, fire is the visible effects from combustion. Combustion occurs when a fuel is heated to its ignition temperature. It has enough available energy to react with oxygen, creating fire. Once started, the fire will keep burning as long as there's enough heat, fuel, and oxygen to sustain it.Depending on the conditions, this is why coal mine fires might burn for centuries. Coal naturally contains its own fuel and oxygen and burns very slowly. Once started, the coals can burn until the carbon source is exhausted.Back to Centralia. Because the fires were limited to the tunnels at first, the 1,000 or so residents found the situation amusing. But other, more dangerous symptoms of the fire soon appeared. Sulfurous fumes and carbon monoxide began seeping out of the ground, nearly suffocating some of the residents in their homes. Even more scary, the fire weakened the ground and left it prone to sinkholes.In 1981, a 12-year-old was crossing a neighbor's yard when an 80 foot sinkhole nearly swallowed him. He was pulled out to safety by one of his cousins. Officials have attempted to put out the fire a few times over the years. One attempt involved dumping wet sand in the holes drilled down from the surface to choke off the air supply. They also tried pumping air supply into the tunnels.In 1992, the state government condemned the city-- the entire city. Today, just a dozen or so people live there. The Centralia fire isn't the only underground coal fire in the world. They're actually alarmingly common, especially in India and China-- which still depend on coal to a high degree and often have lax regulations.Natural underground coal fires also exist. Around the world, there are a few thousand of them burning, compared to about 1,500 active volcanoes. The Centralia fire now reaches as deep as 300 feet and covers some six square miles-- that's more than seven Disneyland's. It's advancing around 75 feet per year along four separate branches and could burn for another 250 years. All the residents of the town may be gone by then, but the coal that brought their ancestors to Pennsylvania in the first place will still be blazing.",
+        image: 'https://rt-homepage.roadtrippers.com/wp-content/uploads/2020/01/centralia-graffiti-highway-5-1920x1280.jpg',
+        credit: "Britannica",
+        creditLink: "https://www.britannica.com/video/187023/coal-mine-fire-Pennsylvania-Centralia"
     },
 
     {
         id: 2,
-        title: 'Fort Benning, Georgia',
-        description: 'lorem asdf daf sadfknl nhiudhfoa whe fnjkdbfjksabf askdfjkabdjkabwofnndvn'
+        title: 'Grand Canyon, Arizona',
+        shortdesc: 'The Grand Canyon is one of the most recognizable places on Earth. Drawing millions of tourists every year from all across the globe...',
+        description: "The Grand Canyon is one of the most recognizable places on Earth. Drawing millions of tourists every year from all across the globe, it is a bucket list item for many people. With a history as grand as the canyon itself, however, even the most frequent visitor doesn’t know everything about this natural marvel. Keep reading for twenty interesting facts about one of the world’s most famous destinations. \n 1. Grand Canyon National Park is bigger than the entire state of Rhode Island.The Park comes in at a massive 1,904 square miles, while Rhode Island is around 1,212 square miles. In some places, the canyon itself is over a mile deep and ten miles across. (Photo by NPS/Grand Canyon National Park) Grand Canyon at Sunset \n 2. The Hopi Tribe considers the Grand Canyon a gateway to the afterlife.The tribe has always placed great spiritual significance on the site. They believe that upon death, a person passes westward through a “place of emergence,” located upstream from the confluence of the Colorado and Little Colorado rivers in the canyon, on his or her journey to the afterlife. \n 3. Temperatures vary greatly within the canyon.From the rim of the canyon to its lowest point, the temperature can change by more than 25 degrees. The depths of the gorge are notoriously hot during the summer, while the North Rim is often below freezing in winter.\n 4. The canyon is full of hidden caves. There are an estimated 1,000 caves within the canyon, but only 335 have been recorded. Only one cave is open to the public. \n 5. In 1909, the canyon was the site of a giant hoax. The Arizona Gazette reported that archaeologists had discovered traces of an ancient Tibetan or Egyptian civilization in an underground tunnel in the canyon. The Smithsonian denied this entire story, claiming that they had no knowledge of the archaeologists. To this day, conspiracy theorists still believe this may have been a government cover-up. \n 6. The Grand Canyon was once home to a booming photo business! The Kolb brothers discovered that they could make money photographing tourists while they were photographing the canyon for themselves. They would take a photo when the tourists departed on mules and sell them back when the riders returned from their journey. The Kolb Studio is located in Grand Canyon Village. \n 7. Despite being the most famous, the Grand Canyon is not actually the world’s deepest canyon.Depending on how the depth of the gorge is measured, the Arizona landmark actually comes behind the Cotahuasi Canyon in Peru and the Kali Gandaki Gorge in Nepal. \n 8. The most dangerous animal in the canyon is actually the rock squirrel!Even though Gila monsters and bighorn sheep also live there, visitors are most often bitten by this humble squirrel. \n 9. The drive from the North Rim Visitor Center to the South Rim Visitor Center is about 200 miles (320km) and takes roughly four hours.This definitely puts the size of this marvel into perspective!\n 10. No one is completely sure about the age of the Grand Canyon. It was long believed that the Colorado River started carving out the canyon six million years ago, but recent studies have shown this process may have actually started 70 million years ago.\n 11. The canyon is full of fossils. Though none of them are from dinosaurs, since the canyon layers were formed long before dinosaurs walked the earth. It is, however, home to fossils of ancient marine animals that date back 1.2 billion years. (NPS photo by Michael Quinn)Grand Canyon fossil bryozoan in redwall limestone by NPS, Michael Quinn \n 12. There’s a town inside the canyon.With a population of 208, Supai Village is the most remote community in the continental United States.\n 13. It’s home to a huge mystery!The Grand Canyon displays a geological phenomenon known as the Great Unconformity. This phenomenon refers to the fact that 250-million-year-old rock layers lie directly against 1.2 billion-year-old rocks. No one knows what happened to the hundreds of millions of years of missing layers. \n 14. The canyon is home to pink snakes. The Grand Canyon Pink Rattlesnake is only found in the Grand Canyon, and is one of six rattlesnake species that live within park boundaries. The snake’s pink color means it blends into the rocks around it, making it extra surprising when a tourist catches a glimpse of one. \n 15. Fewer people have successfully completed a continuous length-wise hike through than Grand Canyon than have walked on the moon. \n 16. The air at the Grand Canyon is among the cleanest air in the United States.Take a deep breath! The elevation of the South Rim is around 7,000 feet (2133m) above seal level. \n 17. It’s still changing shape!The Colorado River flows through the canyon, which, combined with wind and rain, shapes it over time. It’s happening too slowly to see, but it is changing nonetheless. \n 18. The Federal Aviation Administration exists because of the Grand Canyon.In the 1950s, commercial airplanes often took detours over the park to give passengers a look at the marvel. In 1956, however, two of these planes tragically collided. The crash had no survivors, leading the federal government to create the FAA. \n 19. Hundreds of split-twig figurines have been discovered in the canyon walls, shaped like deer and bighorn sheep.Anthropologists believe that prehistoric hunters used these in religious rituals.",
+        image: 'https://www.pinkadventuretours.com/Media/2088/grand-canyon-sunset.jpg',
+        credit: "PinkAdventureTours",
+        creditLink: 'https://www.pinkadventuretours.com'
     },
 
     {
         id: 3,
-        title: 'Jacobs Hole, Texas',
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, cupiditate.'
+        title: 'Lake Tahoe, Nevada',
+        shortdesc: 'Lake Tahoe is the stunning jewel of the Sierra with its vibrant and famous green to blue hues and iconic granite boulders, Lake Tahoe straddles the California-Nevada border, ...',
+        description: 'Lake Tahoe is the stunning jewel of the Sierra with its vibrant and famous green to blue hues and iconic granite boulders, Lake Tahoe straddles the California-Nevada border, sits at 6,225 feet above sea level, and is surrounded by the majestic peaks of the Sierra Nevada and Carson Ranges. The surrounding mountain ranges vary in elevation from 9,000 to 11,000 feet. Lake Tahoe is the largest alpine lake in North America, and the second deepest lake in the U.S., with Crater Lake in Oregon being the deepest in the U.S. Lake Tahoe measures to 1,645 feet deep with an average depth of 1,000 feet. Lake Tahoe contains 39 trillion gallons of water, enough to cover the state of California 14 inches. The North Shore of Lake Tahoe is a stunning 180 degree panorama calling more than romantics and outdoor enthusiasts. Scientists journey to this place to understand its incredible blueness and ensure that it remains an icon of pure alpine perfection. Whether your trip to North Lake Tahoe is about academic study, or simply with the hope of taking a few well-deserved days to enjoy the radiant light, we recommend exploring our towns and reveling in the multiple variations of fun found only in North Lake Tahoe. Cultural history fun fact, the name Tahoe comes from a mispronunciation of the Washoe People’s (Wašiw) name for Lake Tahoe, da.aw. The Washoe People spent summers at Da.aw and all parts of the territory. Once plentiful, Large Cutthroat trout lived in all the lakes and streams along with freshwater clams which sustained the Washoe People. Large and small game also was once plentiful. Plant gathering for food, utilitarian and medicinal use is still harvested in all parts of Wašiw lands today.',
+        image: "https://www.worldatlas.com/upload/b9/b5/1f/lake-tahoe-california-nevada-us-nkneidlphoto.jpg",
+        credit: "GoTahoeNorth",
+        creditLink: "https://www.gotahoenorth.com/lake-tahoe/"
     },
 
     {
         id: 4,
-        title: 'Mount Ranier, Washington',
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, cupiditate.'
-    },
-
-    {
-        id: 5,
-        title: 'Ben & Jerry\'s Flavor Graveyard, Vermont',
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, cupiditate.'
+        title: 'Yellowstone, Wyoming',
+        shortdesc: "Yellowstone was established on March 1, 1872. It is the world's first National Park. It contains 3,472 square miles and measures 63 miles north to south and 54 miles east to west...",
+        description: " Yellowstone was established on March 1, 1872. It is the world's first National Park. It contains 3,472 square miles and measures 63 miles north to south and 54 miles east to west. The park is contained in three states: 96% in Wyoming, 3% in Montana, and 1% in Idaho. The highest point in the park is 11,358' at Eagle Peak and the lowest point in the park is 5,282' at Reese Creek. Yellowstone is larger than the states of Rhode Island and Delaware combined.Yellowstone has 1000 to 3000 earthquakes annually.There are more than 300 active geysersThere are more than 290 waterfalls.Yellowstone is home to 67 species of mammals, 285 species of birds, 16 species of fish, 6 species of reptiles, 5 species of amphibians, and more than 7 aquatic nuisance species.Two threatened species call the park home - Canada lynx and Grizzly bears. Wolverines are a proposed threatened species.Yellowstone has more than 1800 archaeological sites.There are five park entrances and 466 miles of roads.There are 15 miles of boardwalk.There are 92 trailheads that access approximately 1000 miles of trails.In 2015, Yellowstone National Park had 4,097,710 visits - a park record.Old Faithful, a world-famous geyser, erupts every 92 minutes on average.",
+        image: "https://www.visitbigsky.com/sites/default/files/styles/scale_1280/public/2021-04/Yellowstone%20Tours.jpg?itok=NoraLPPR",
+        credit: "VisitBigSky",
+        creditLink: "https://www.visitbigsky.com/get-inspired/big-sky-stories/17-fun-facts-about-yellowstone-national-park#:~:text=Yellowstone%20was%20established%20on%20March,the%20world's%20first%20National%20Park.&text=The%20park%20is%20contained%20in,is%205%2C282'%20at%20Reese%20Creek."
     },
 ]
 
 
 const Section = () => {
+
 
     return( 
         <section id='section' className="section">
@@ -44,19 +56,19 @@ const Section = () => {
 
                     {places.map((place) => {
                         return(
-                            <div>
-                            <div className="info">
+                            <div key={place.id}>
+                            <div className="info" >
                                 <h2 className="title">{place.title}</h2>
                                 <div className="description">
                                     <p>
-                                        {place.description}
+                                        {place.shortdesc}
                                     </p>
                                 </div>
                                 <div className="button-div">
-                                    <Button buttonType='primary' type='button'>Test Button</Button>
+                                    <Link to='/page' state={{place}}><Button buttonType='primary' type='button'>Learn More</Button></Link>
                                 </div>
                             </div>
-                                <img  src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                                <img  src={place.image} alt="" />
                             </div>
                         );
                     })}
